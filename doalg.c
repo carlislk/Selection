3be5acdcd76ab17331b165d8c5a56d63bb529a62
Jared Napoli <Jared@Jareds-MacBook-Air.local>
2016-04-13 02:28:38 -0700

    cleaned up the code, added citation

--- doalg.c
+++ doalg.c
@@ -1,8 +1,14 @@
 // Do algorithm Routine
+// Kevin Carlisle 82682616
+// Jared Napoli 31666553
+// binarySearch algorithm adapted from Jeffery Stedfast 
+// at http://jeffreystedfast.blogspot.com/2007/02/binary-insertion-sort.html
 #include <stdio.h>
 
 int add_ordered(int i, int k, int* best);
 int compareAndSwap(int i , int j, int k, int* best);
+int getPos(int k, int* best);
+void swapAtP(int p, int k, int* best);
 
 
 
@@ -13,49 +19,15 @@ int doalg(int n, int k, int* best)
 
 	// Initialize best array to -1
 	int i; 
-	//printf("before that!\n");
 	for ( i = 0; i < k; i++) 
 	{ 
-		//printf("%d\n", i);
 		best[i] = -1; 
 	}
 
-	// Iterate over each index in array and test to see if it belongs in best
-	//printf("before\n");
-	for ( i = 1; i <= k; i++ ) 
+	for ( i = 1; i <= n; i++ )
 	{
 		add_ordered(i, k, best);
-		//printf("%d\n", i);
 	}
-	//printf("After add ordered1\n");
-	// // // print out shit
-	// for ( i = 1; i < k; i++)
-	// {
-	// 	printf("%d\n", best[i]);
-	// 	COMPARE(-69, best[i]);
-	// }
-
-	// COMPARE(-68,11);
-
-
-	for ( i = k+1; i <= n; i++ )
-	{
-		add_ordered(i, k, best);
-		//printf("%d\n", i);
-	}
-	//printf("after add ordered2\n");
-
-	// // print out shit
-	// for ( i = 0; i < k; i++)
-	// {
-	// 	//printf("%d\n", best[i]);
-	// 	COMPARE(-69, best[i]);
-	// }
-
-
-
-
-
 
 	return 1;
 }
@@ -67,7 +39,7 @@ int add_ordered( int outsideIndex, int k, int* best)
 
 
 	// Compares Index values to see if outsideIndex belongs in best array
-		// Starts at end of best array * Lowest Values
+	// Starts at end of best array * Lowest Values
 	if ( best[k-1] == -1 || COMPARE(best[k-1], outsideIndex) == 2 )
 	{
 		best[k-1] = outsideIndex;
@@ -78,34 +50,20 @@ int add_ordered( int outsideIndex, int k, int* best)
 		return 0;
 	}
 
+	// Index value was placed in last index of aray now we need to 
+	// determine where it belongs in the sorted array
+	int p = getPos(k, best);
 
+	// Insert Last element into P place and move the rest of the array down one index
+	swapAtP(p, k, best);
 
-	// If Index value was placed in last index of array now percolate up.
-	int i;
-
-	for ( i = k-1; i > 0; i--)
-	{
-		//printf("i: %d, j: %d\n", i-1, i);
-		if ( compareAndSwap(i-1, i, k, best) == 0 )
-		{
-			// if compareAndSwap returns 0 the value was not swapped
-			// no further comparisons need to be made
-			//printf("Broke Early\n");
-			return 1; 
-		}
-	}
-
-	//printf("Added to 0 index\n");
-
-
+	return 1;
 }
 
 int compareAndSwap(int i, int j, int k, int* best)
 {
 	if ( best[i] == -1 || COMPARE(best[i], best[j]) == 2 )
 	{
-		//printf("                Swap\n");
-
 		int temp = best[i];
 		best[i] = best[j];
 		best[j] = temp;
@@ -118,4 +76,58 @@ int compareAndSwap(int i, int j, int k, int* best)
 		return 0;
 	}
 
+}
+
+// adapted from Jeffery Stedfast 
+// at http://jeffreystedfast.blogspot.com/2007/02/binary-insertion-sort.html
+int binarySearch (int* best, int low, int high, int key)
+{
+	int mid;
+
+	if ( low == high ) { return low; }
+
+	mid = low + ((high-low) / 2);
+
+	//else if (key < best[mid])
+	if (best[mid] == -1 || COMPARE(best[key], best[mid]) == 1)
+	{
+		return  binarySearch( best, low, mid, key);
+	}
+	//if (key > best[mid])
+	else if ( COMPARE(best[key], best[mid]) == 2)
+	{
+		return binarySearch(best, mid+1, high, key);
+	}
+	return mid;
+}
+
+int getPos(int k, int* best)
+{
+	// Determines where to place last item in array
+	// Array is currently sorted except for the last value
+	return binarySearch(best, 0, k-1, k-1);
+}
+
+void swapAtP ( int p, int k,  int* best)
+{
+	// Swap Last element to place p
+	// Move elements of array down
+
+	// Add check if no swap needed
+
+	int temp = best[k-1];
+
+	int i;
+
+	if(p == k-1)
+	{
+		return;
+	}
+
+	for ( i = k-1; i > p; i--)
+	{
+		best[i] = best[i-1];
+	}
+
+	best[p] = temp;
 }
